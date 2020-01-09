464739f33a5b97f69c7f2c69beb34223a405353b
Renier Trenuela <>
2020-01-09 15:42:18 +0800

    New error 403 layout

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="id" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <!-- Title -->
+    <title>Sorry, This Page Can&#39;t Be Accessed</title>
+    <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-dark text-white py-5">
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-2 text-center">
+            <p><i class="fa fa-exclamation-triangle fa-5x"></i><br/>Status Code: 403</p>
+        </div>
+        <div class="col-md-10">
+            <h3>This section is not available for you.</h3>
+            <p>Sorry, your access is refused due to security reasons of our server and also our sensitive data.
+                <br/>Please go back to the previous page to continue browsing or report to Administrator.</p>
+            <a class="btn btn-danger" href="javascript:history.back()">Go Back</a>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
