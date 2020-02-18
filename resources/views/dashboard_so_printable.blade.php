1df46c80c1a6347fc806ab7a666fac9b27295d02
Renier Trenuela <>
2020-02-18 15:08:48 +0800

    Separate QUote and shipped

--- resources/views/dashboard_so_printable.blade.php
+++ resources/views/dashboard_so_printable.blade.php
@@ -1,16 +1,42 @@
+<h2>SHIPPED</h2>
 <table border="1">
     <thead>
         <th>SO No</th>
         <th>Subject</th>
+        <th>Status</th>
         <th>grand_total</th>
     </thead>
     <tbody>
         @foreach ($data as $item)
-        <tr>
-            <td>{{ $item->so_no }}</td>
-            <td>{{ $item->subject }}</td>
-            <td>{{ $item->grand_total }}</td>
-        </tr>
+        @if($item->status == 'Shipped')
+           <tr>
+           <td>{{ $item->so_no }}</td>
+           <td>{{ $item->subject }}</td>
+           <td>{{ $item->status }}</td>
+           <td>{{ $item->grand_total }}</td>
+           </tr>
+       @endif
         @endforeach
     </tbody>
-    </table>
+</table>
+<h2>Quote</h2>
+<table border="1">
+    <thead>
+        <th>SO No</th>
+        <th>Subject</th>
+        <th>Status</th>
+        <th>grand_total</th>
+    </thead>
+    <tbody>
+        @foreach ($data as $item)
+         @if($item->status == 'Quote')
+            <tr>
+            <td>{{ $item->so_no }}</td>
+            <td>{{ $item->subject }}</td>
+            <td>{{ $item->status }}</td>
+            <td>{{ $item->grand_total }}</td>
+            </tr>
+        @endif
+        @endforeach
+    </tbody>
+</table>
