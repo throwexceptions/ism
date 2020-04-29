5f91d8d861544af0f568bde9970ab6f36a54764e
Renier Trenuela <renier.trenuela@gmail.com>
2020-04-29 20:48:58 +0800

    adjust recalibrate

--- app/Supply.php
+++ app/Supply.php
@@ -22,24 +22,30 @@ class Supply extends Model
 
     public static function recalibrate()
     {
-        $product_id = self::query()->get('product_id')->toArray();
+        $data = self::query()
+            ->select(['product_id', 'type'])
+            ->join('products','products.id', 'supplies.product_id')
+            ->get()
+            ->toArray();
 
-        foreach ($product_id as $id) {
-            $so = DB::table('sales_orders')
-                ->join('product_details', 'product_details.sales_order_id', '=', 'sales_orders.id')
-                ->where('sales_orders.status', 'Shipped')
-                ->where('product_id', $id)
-                ->sum('product_details.qty');
+        foreach ($data as $value) {
+            $real_val = 0 ;
+            if($value['type']) {
+                $so = DB::table('sales_orders')
+                    ->join('product_details', 'product_details.sales_order_id', '=', 'sales_orders.id')
+                    ->where('sales_orders.status', 'Shipped')
+                    ->where('product_id', $value['product_id'])
+                    ->sum('product_details.qty');
 
-            $po = DB::table('purchase_infos')
-                ->join('product_details', 'product_details.purchase_order_id', '=', 'purchase_infos.id')
-                ->where('purchase_infos.status', 'Received')
-                ->where('product_id', $id)
-                ->sum('product_details.qty');
+                $po = DB::table('purchase_infos')
+                    ->join('product_details', 'product_details.purchase_order_id', '=', 'purchase_infos.id')
+                    ->where('purchase_infos.status', 'Received')
+                    ->where('product_id', $value['product_id'])
+                    ->sum('product_details.qty');
 
-            $real_val = $po - $so;
-
-            self::query()->where('product_id', $id)->update(['quantity' => $real_val]);
+                $real_val = $po - $so;
+            }
+            self::query()->where('product_id', $value['product_id'])->update(['quantity' => $real_val]);
         }
     }
 }
