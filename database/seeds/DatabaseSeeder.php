62e79465b5daf75fc74a7c3d0c18e9f95af1702d
Renier Trenuela <renier.trenuela@gmail.com>
2020-05-13 20:40:11 +0800

    create initialization

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,25 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Category::truncate();
-        \App\Category::query()->insert([
-            ['type' => 'SERVICES', 'name' => 'CCTV BGMPA'],
-            ['type' => 'SERVICES', 'name' => 'FDAS'],
-            ['type' => 'SERVICES', 'name' => 'STRUCTURED CABLING'],
-            ['type' => 'SERVICES', 'name' => 'COMPUTERS'],
-            ['type' => 'SERVICES', 'name' => 'ACCESSORIES'],
-            ['type' => 'SERVICES', 'name' => 'DOOR ACCESS'],
-        ]);
-
-        $this->call(ProductTableSeeder::class);
-        $this->call(VendorTableSeeder::class);
-        $this->call(CustomerTableSeeder::class);
-        $this->call(SupplyTableSeeder::class);
-        $this->call(PurchaseInfoTableSeeder::class);
-        $this->call(SalesOrderTableSeeder::class);
-        $this->call(ProductDetailTableSeeder::class);
-        $this->call(SummaryTableSeeder::class);
-        $this->call(OrderFormTableSeeder::class);
+//        \App\Category::truncate();
+//        \App\Category::query()->insert([
+//            ['type' => 'SERVICES', 'name' => 'CCTV BGMPA'],
+//            ['type' => 'SERVICES', 'name' => 'FDAS'],
+//            ['type' => 'SERVICES', 'name' => 'STRUCTURED CABLING'],
+//            ['type' => 'SERVICES', 'name' => 'COMPUTERS'],
+//            ['type' => 'SERVICES', 'name' => 'ACCESSORIES'],
+//            ['type' => 'SERVICES', 'name' => 'DOOR ACCESS'],
+//        ]);
+//
+//        $this->call(ProductTableSeeder::class);
+//        $this->call(VendorTableSeeder::class);
+//        $this->call(CustomerTableSeeder::class);
+//        $this->call(SupplyTableSeeder::class);
+//        $this->call(PurchaseInfoTableSeeder::class);
+//        $this->call(SalesOrderTableSeeder::class);
+//        $this->call(ProductDetailTableSeeder::class);
+//        $this->call(SummaryTableSeeder::class);
+//        $this->call(OrderFormTableSeeder::class);
+        $this->call(SuperAdminTableSeeder::class);
         $this->call(PreferenceTableSeeder::class);
     }
 }
